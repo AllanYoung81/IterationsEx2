590d8df3c6385c6fc7c78618079962c8b02ca883
Allan Young <ayounggtr@gmail.com>
2023-11-09 07:01:57 -0500

    Iterations Syntax Drill

---
+++ IterationsEx2/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace IterationsEx2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+//1
+        for (int i = 0; i < 5; i++)
+        {
+           Console.WriteLine($"{i}") ;
+        }
+//2
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//3
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//4
+        for (int i = 0; i <5; i++)
+        {
+           Console.WriterLine($"{i}");
+        }
+//5
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//6        
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//7
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLIne($"{i}");
+        }
+//8
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//9
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//10
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//11    
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//12
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//13
+        for (int i = 0; I < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//14    
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//15    
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//16
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//17
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//18    
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+//19    
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WrtiteLine($"{i}");
+        }
+//20
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+                    }
+
+    }
+}
